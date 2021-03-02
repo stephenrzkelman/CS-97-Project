50a7309c546702ee309b0a60d4dfe1602b231964
PaulSera1 <paulserafimescu@gmail.com>
2021-03-02 10:11:30 -0800

    router

--- filler-name/src/App.js
+++ filler-name/src/App.js
@@ -1,16 +1,36 @@
-import {
-  Profile
-} from './components';
-import {
-  useState
-} from 'react';
+import './App.css';
 
+import Navigation from './Navigation';
+import Profile from './Profile';
+import Home from './Components/Home';
+import Calendar from './Calendar';
+import Login from './Login';
+import Logout from './Logout';
+import Workout from './Workout';
+import FeedPost from './Components/FeedPost';
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+
+
+//what is shown on the webpage
 function App() {
-
-  const [JWT, setJWT] = useState(window.localStorage.getItem('jwt'));
-
   return (
-    <Profile jwt={JWT} />
+    <div className="App">
+        <Router>
+        <Navigation />
+          <Switch>
+          <Route path="/home" exact component={Home} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/calendar" component={Calendar} />
+          <Route path="/login" component={Login} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/feed" component={FeedPost} />
+          <Route path="/workout" component={Workout} />
+          </Switch>
+      </Router>
+
+    </div>
   );
 }
 
